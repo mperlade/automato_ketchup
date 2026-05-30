021b6f354182182908a92a55962ca5feefd18937
marc <marc.perlade@ens.psl.eu>
2026-05-31 00:31:09 +0200

    Epsilon NFA def

---
+++ AutoKchp/NatEpsNFA.lean
@@ -0,0 +1,35 @@
+/-
+Copyright (c) 2026 Marc Perlade. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Marc Perlade
+-/
+
+module
+
+@[expose]
+public section
+
+structure NatEpsNFA (a: Nat) where
+  n: Nat
+  δ: Fin n → Option (Fin a) → (Array (Fin n))
+  i: Fin n → Bool
+  f: Fin n → Bool
+
+
+namespace NatEpsNFA
+
+inductive path {a: Nat} (r: NatEpsNFA a): Fin r.n → Fin r.n → List (Fin a) → Prop where
+  | refl (p: Fin r.n): r.path p p []
+  | trans (b: Fin a) (t: List (Fin a)) (p q u: Fin r.n) (h1: u ∈ r.δ p (some b)) (h2: r.path u q t): r.path p q (b::t)
+  | eps (l: List (Fin a)) (p q u: Fin r.n) (h1: u ∈ r.δ p none) (h2: r.path u q l): r.path p q l
+
+
+def successfulPath {a: Nat} (r: NatEpsNFA a) (p q: Fin r.n) (l: List (Fin a)): Prop :=
+  r.path p q l ∧ r.i p ∧ r.f q
+
+
+def accepts {a: Nat} (r: NatEpsNFA a) (l: List (Fin a)): Prop :=
+  ∃ p q: Fin r.n, r.successfulPath p q l
+
+end NatEpsNFA
+end
